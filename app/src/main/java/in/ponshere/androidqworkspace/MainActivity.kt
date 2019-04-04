c077d3ebfffafdc5162ed1c3e546cbe989023ca0
Ponsuyambu <suyambu.vps@gmai.com>
2019-04-04 04:00:40 +0200

    Add notification helper and add bubble activity

--- app/src/main/java/in/ponshere/androidqworkspace/MainActivity.kt
+++ app/src/main/java/in/ponshere/androidqworkspace/MainActivity.kt
@@ -1,12 +1,21 @@
 package `in`.ponshere.androidqworkspace
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android.bubbles.data.NotificationHelper
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("WrongThread")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NotificationHelper(this). apply {
+            setUpNotificationChannels()
+            showNotification("Test", false)
+        }
+
     }
 }
